bb8102c7f88f5aae5aa54528cc5393532f6c5243
Dishant Rathi <dishant16rathi@gmail.com>
2021-02-02 17:01:18 +0530

    database changed to mongodb

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,13 +1,15 @@
-import {inject} from '@loopback/core';
 import {DefaultCrudRepository, juggler} from '@loopback/repository';
 import {User, UserRelations} from '../models';
+import {inject} from '@loopback/core';
 
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id,
   UserRelations
 > {
-  constructor(@inject('datasources.db') dataSource: juggler.DataSource) {
+  constructor(
+    @inject('datasources.db') dataSource: juggler.DataSource,
+  ) {
     super(User, dataSource);
   }
 }
